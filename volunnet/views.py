0340b59a107beadd1b04f2b3d1647e02b8e49f02
blai <blai@unomaha.edu>
2019-03-12 18:57:01 -0500

    remove views.py errors commit 2

--- volunnet/views.py
+++ volunnet/views.py
@@ -2,13 +2,9 @@ from django.shortcuts import render
 from .models import *
 from .forms import *
 from django.contrib.auth.decorators import login_required
-
-<<<<<<< HEAD
-
-@login_required
-=======
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+
 
 now = timezone.now()
 def home(request):
@@ -16,16 +12,12 @@ def home(request):
                  {'volunnet': home})
 
 
->>>>>>> ab92afad97668fc316e45de48a75c10dac67e8e7
 def activity_list(request):
     activity = Activity.objects.filter(published_date__lte=timezone.now())
     return render(request, 'volunnet/activity_list.html',
                  {'activities': activity})
 
-<<<<<<< HEAD
 
-@login_required
-=======
 @login_required
 def activity_new(request):
    if request.method == "POST":
@@ -42,7 +34,6 @@ def activity_new(request):
    return render(request, 'volunnet/activity_new.html', {'form': form})
 
 
->>>>>>> ab92afad97668fc316e45de48a75c10dac67e8e7
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
