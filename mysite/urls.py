63b424c7d9712d6ead683db67ae9301fb68407cc
cvong1001 <cvong@unomaha.edu>
2019-03-11 23:05:20 -0500

    Forgot password template information

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('volunnet.urls')),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name="login"),
     path('logout/', LogoutView.as_view(), LogoutView.next_page, name="logout"),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
